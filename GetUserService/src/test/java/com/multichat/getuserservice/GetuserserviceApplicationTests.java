5200d529d18a99a5e75c5cef4d7b8c1e663071ac
MXW9699 <mitchell.wen@hotmail.com>
2024-04-02 18:40:33 -0400

    ok

---
+++ GetUserService/src/test/java/com/multichat/getuserservice/GetuserserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.multichat.getuserservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GetuserserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
